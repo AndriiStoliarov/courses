b22a8f1ac167e6e8b5e6798ff016f94142374d2c
Andrii Stoliarov <andrey159852@gmail.com>
2025-06-27 20:34:31 +0300

    homework 23 task 3 added

--- freelancerLifeStyle/spring-2025/lessons/lesson-23/homework/js/script.js
+++ freelancerLifeStyle/spring-2025/lessons/lesson-23/homework/js/script.js
@@ -1,2 +1,46 @@
 "use strict";
 
+// Задача №1.
+// Отримати в константу елемент <body>.
+
+const bodyElement = document.body;
+if (bodyElement) {
+  console.log('document.body :>> ', bodyElement);
+}
+
+// Задача №2.
+// Написати функцію, яка додає в <body> список UL
+// з певною кількістю LI (кількість має передаватись як параметр функції, також мати значення за замовченням).
+
+function addList(amountListItem = 2) {
+  let bodyElement2 = document.body;
+  let ulElement = document.createElement('ul');
+
+  if (bodyElement2) {
+    for (let i = 0; i < amountListItem; i++) {
+      const liElement = document.createElement('li');
+
+      ulElement.append(liElement);
+    }
+
+    bodyElement.insertAdjacentElement('afterbegin', ulElement);
+  }
+}
+
+addList(6);
+
+// Задача №3.
+// Додати до елементу <body> класс loaded.
+// Потім перевірити чи є клас loaded у елемента <body>
+// і, якщо є, додати стиль кольору тесту зелений.
+
+let bodyElement3 = document.body;
+
+if (bodyElement3) {
+  bodyElement3.classList.add('loaded');
+  const isLoaded = bodyElement3.classList.contains('loaded');
+
+  if (isLoaded) {
+    bodyElement3.style.color = 'green';
+  }
+}
