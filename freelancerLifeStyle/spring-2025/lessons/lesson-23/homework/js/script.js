f34709fcde880383cadb303acb34d315213ad2c1
Andrii Stoliarov <andrey159852@gmail.com>
2025-06-28 18:54:29 +0300

    homework 23 completed

--- freelancerLifeStyle/spring-2025/lessons/lesson-23/homework/js/script.js
+++ freelancerLifeStyle/spring-2025/lessons/lesson-23/homework/js/script.js
@@ -9,11 +9,10 @@ if (bodyElement) {
 }
 
 // Задача №2.
-// Написати функцію, яка додає в <body> список UL
-// з певною кількістю LI (кількість має передаватись як параметр функції, також мати значення за замовченням).
+// Написати функцію, яка додає в <body> список UL з певною кількістю LI (кількість має передаватись як параметр функції, також мати значення за замовченням).
 
 function addList(amountListItem = 2) {
-  let bodyElement2 = document.body;
+  const bodyElement2 = document.body;
   let ulElement = document.createElement('ul');
 
   if (bodyElement2) {
@@ -31,10 +30,9 @@ addList(6);
 
 // Задача №3.
 // Додати до елементу <body> класс loaded.
-// Потім перевірити чи є клас loaded у елемента <body>
-// і, якщо є, додати стиль кольору тесту зелений.
+// Потім перевірити чи є клас loaded у елемента <body> і, якщо є, додати стиль кольору тесту зелений.
 
-let bodyElement3 = document.body;
+const bodyElement3 = document.body;
 
 if (bodyElement3) {
   bodyElement3.classList.add('loaded');
@@ -42,5 +40,52 @@ if (bodyElement3) {
 
   if (isLoaded) {
     bodyElement3.style.color = 'green';
+  }
+}
+
+// Задача №4.
+// Дано в html: три елементи з класом item.
+// Треба отримати ці елементи в константу, кожному додати клас active та перезаписати контент всередені кожного елемента на "Елемент №(тут порядковий номер елементу починаючи з 1)".
+
+const itemElements = document.querySelectorAll('.item');
+
+if (itemElements.length) {
+  itemElements.forEach((item, index) => {
+    item.classList.add('active');
+    item.textContent = `Елемент № ${index + 1}`;
+  });
+}
+
+// Задача №5.
+// Дано в html: текст, далі кнопка з класом button.
+// Треба прокрутити скрол сторінки до кнопки.
+
+const button = document.querySelector('.button');
+
+function scrollToBlock(element) {
+  element.scrollIntoView({
+    block: "center",
+    inline: "nearest",
+    behavior: "smooth"
+  });
+}
+
+if (button) {
+  scrollToBlock(button);
+}
+
+// Задача №6.
+// Дано в html: посилання з класом link.
+// Треба додати до посилання дата атрибут зі значенням 100. Поім отримати значення трибуту, та, якщо значення меньше 200 пофарбувати колір тексту посилання в червоний.
+
+const link = document.querySelector('.link');
+
+if (link) {
+  link.dataset.linkAttr = '100';
+
+  const linkAttrValue = +link.dataset.linkAttr;
+
+  if (linkAttrValue < 200) {
+    link.style.color = 'red';
   }
 }
