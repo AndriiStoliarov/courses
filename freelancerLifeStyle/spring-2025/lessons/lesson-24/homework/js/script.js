c74c0390833ca828c2248d098fae73b0a49abbdf
Andrii Stoliarov <andrey159852@gmail.com>
2025-07-01 19:46:37 +0300

    homework 24 task 3 added

--- freelancerLifeStyle/spring-2025/lessons/lesson-24/homework/js/script.js
+++ freelancerLifeStyle/spring-2025/lessons/lesson-24/homework/js/script.js
@@ -37,18 +37,43 @@ console.log('document.elementFromPoint(550, 0) :>> ', document.elementFromPoint(
 // ===============================================================
 
 // Задача №1.
+// Дано в html: три елементи з класом item.
+// При кліку на кожен з елментів додати клас active, при повторному кліку прибрати клас.
 
+const itemElements = document.querySelectorAll('.item');
+
+if (itemElements.length) {
+  itemElements.forEach(itemElement => {
+    itemElement.addEventListener("click", () => document.documentElement.classList.toggle('active'));
+  });
+}
 
 // Задача №2.
+// Дано в css/scss: body прозорий.
+// При повному завантаженню сторінки додати клас до body, який прибирає прозорість.
 
+window.addEventListener("load", () => document.body.classList.add('body--opacity-off'));
 
-// Задача №3.
+//? Задача №3.
+// Дано в html: header main footer.
+// При наведенні курсору на header змінювати колір фону у footer. Коли курсор йде з header повертати початковий фон для footer.
 
+//? background animation
+const headerElement = document.querySelector('header');
+
+if (headerElement) {
+  headerElement.addEventListener("mouseenter", changeFooterColor);
+  headerElement.addEventListener("mouseleave", changeFooterColor);
+}
+
+function changeFooterColor() {
+  const footerElement = document.querySelector('footer');
+
+  if (footerElement) {
+    footerElement.classList.toggle('footer--bg-color');
+  }
+}
 
 // Задача №4.
-
-
-// Задача №5.
-
-
-// Задача №6.
+// Дано в html: текст, елемент з класом item, текст. Так, що елемент з класом item за межами в'юпотрта.
+// Створити функцію, яка будує інтервал, який буде змінювати контент в елементі item, виводячи цифру, яка збільшується на одиницю: 1 2 3 ... і т.д. Затримка між зміною числа, та до якого числа має працювати інтервал має задаватись в дата атрибутах елемента item.Функція має запускатить коли ми доскролюємо до елементу item (його видно), і не запускатись повторно.
