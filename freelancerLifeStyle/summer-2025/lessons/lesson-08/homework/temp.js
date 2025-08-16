aad7c64e37f3c967bfc11ebb51aa0362659c479d
Andrii Stoliarov <andrey159852@gmail.com>
2025-08-16 20:58:43 +0300

    homework 08 task 6 added

--- freelancerLifeStyle/summer-2025/lessons/lesson-08/homework/temp.js
+++ freelancerLifeStyle/summer-2025/lessons/lesson-08/homework/temp.js
@@ -1,59 +1,115 @@
-const subjectAmount = parseInt(prompt("Введіть кількість предметів:", "10"));
+const productsTitles = [
+  "сир",
+  "сок",
+  "хлів",
+  "банани",
+  "молоко",
+  "картопля",
+  "малина",
+  "масло",
+  "помідори",
+  "шоколад",
+];
 
-const gradsArray = createArray(subjectAmount);
-const averageGrade = getAverageGrade(gradsArray);
-const gradeStatus = getAverageGrade(averageGrade);
+const productsPrices = [123, 41, 25, 38, 64, 32, 200, 178, 94, 50];
 
-function getGradeStatus(averageGrade) {
-  let gradeStatus = "";
+const moneyAmount = parseInt(prompt("Введіть кількість грошей:", "500"));
 
-  switch (averageGrade) {
-    case 5:
-      gradeStatus = "Відмінник.";
-      break;
+const productsAmount = getProductsAmount(productsPrices, moneyAmount);
+const productsTitlesUserCan = getProductsTitles(productsAmount);
 
-    case 4:
-      gradeStatus = "Хорошист.";
-      break;
+document.write(`
+  <li class="list__item">
+    <span>Користувач може собі дозволити:</span>
+  </li>
+`);
 
-    case 3:
-      gradeStatus = "Трієчник.";
-      break;
+// функція повертає перелік товарів
+function getProductsTitles(productsTitles, productsAmount) {
+  let productsTitlesArray = [];
 
-    case 2:
-    case 1:
-      gradeStatus = "Двієчник.";
-      break;
-
-    default:
-      throw new Error("Учня вигнали зі школи.");
+  for (let i = 0; i < productsAmount; i++) {
+    productsTitlesArray.push(productsTitles[i]);
   }
 
-  return gradeStatus;
+  return productsTitlesArray;
 }
 
-function getAverageGrade(gradesArray) {
+// функція повертає кількість продуктів
+function getProductsAmount(productsPrices, moneyAmount) {
+  let currentMoneyAmount = moneyAmount;
+  let counterProducts = 0;
+
+  for (let i = 0; i < productsPrices.length; i++) {
+    if (currentMoneyAmount > 0 && currentMoneyAmount > array[i]) {
+      currentMoneyAmount -= productsPrices[i];
+
+      if (currentMoneyAmount <= 0) {
+        break;
+      } else {
+        counterProducts++;
+      }
+    }
+  }
+
+  return counterProducts;
+}
+
+// функція, яка виводить масив
+function showArray(array) {
+  document.write(`
+    <li class="list__item">
+      <span>Массив оцінок: [</span>
+  `);
+
+  for (let index = 0; index < array.length; index++) {
+    document.write(`
+        <span>${array[index]}</span>
+    `);
+  }
+
+  document.write(`
+      <span>].</span>
+    </li>
+  `);
+}
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// функція повертає кількість двійок
+function getTwoAmount(array) {
+  let counterTwo = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 2) counterTwo++;
+  }
+
+  return counterTwo;
+}
+
+// функція повертає середній бал
+function getAverageMark(marksArray) {
   let gradesSum = 0;
 
   for (let grade = 0; grade < gradesArray.length; grade++) {
-    averageGrade += gradesArray[grade];
+    gradesSum += gradesArray[grade];
   }
 
   const averageGrade = gradesSum / gradesArray.length;
 
-  return averageGrade;
+  return Math.round(averageGrade);
 }
 
+// функція повертає масив оцінок
 function createArray(sizeArray) {
   let array = [];
 
-  for (let index = 0; index < sizeArray.length; index++) {
-    array[index].push(generateRandomNumber(1, 5));
+  for (let index = 0; index < sizeArray; index++) {
+    array.push(generateRandomNumber(2, 5));
   }
 
   return array;
 }
 
+// функція повертає випадкове число в заданому інтервалі
 function generateRandomNumber(startRange, endRange) {
   const RANDOM_NUMBER_TO = startRange,
     RANDOM_NUMBER_FROM = endRange;
