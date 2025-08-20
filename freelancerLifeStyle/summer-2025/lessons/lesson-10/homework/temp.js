02e5f41d0c89847ca723e6c2a7aaaca7c6b48af9
Andrii Stoliarov <andrey159852@gmail.com>
2025-08-20 14:25:15 +0300

    homework 10 task 05 added

--- freelancerLifeStyle/summer-2025/lessons/lesson-10/homework/temp.js
+++ freelancerLifeStyle/summer-2025/lessons/lesson-10/homework/temp.js
@@ -1,10 +1,62 @@
-parseInt(prompt("Введіть кількість елементів:", "10"));
+const elements = parseInt(prompt("Введіть кількість елементів:", "10"));
 
-// функція повертає масив з вказаної кількості елементів заповнених нулями.
-function createArrayFillZero(sizeArray) {
-  const array = new Array(sizeArray);
+const randomNumbers = createArray(elements);
 
-  array.fill(0);
+showArray(randomNumbers);
+
+const numbersBigger100 = getNumbersBigger100(randomNumbers);
+
+showArray(numbersBigger100);
+
+// функція повертає масив елементів, що більші за 100
+function getNumbersBigger100(numbers) {
+  let array = [];
+
+  for (let number of numbers) {
+    if (number > 100) array.push(number);
+  }
 
   return array;
+}
+
+// функція повертає масив випадкових елементів
+function createArray(sizeArray) {
+  let array = [];
+
+  for (let index = 0; index < sizeArray; index++) {
+    array.push(generateRandomNumber(0, 200));
+  }
+
+  return array;
+}
+
+// функція повертає випадкове число в заданому інтервалі
+function generateRandomNumber(startRange, endRange) {
+  const RANDOM_NUMBER_TO = startRange,
+    RANDOM_NUMBER_FROM = endRange;
+
+  const randomNumber =
+    RANDOM_NUMBER_TO +
+    Math.floor(Math.random() * (RANDOM_NUMBER_FROM - RANDOM_NUMBER_TO + 1));
+
+  return randomNumber;
+}
+
+// функція, яка виводить масив
+function showArray(array, caption) {
+  document.write(`
+    <li class="list__item">
+      <span>${caption}: [</span>
+  `);
+
+  for (let index = 0; index < array.length; index++) {
+    document.write(`
+        <span>${array[index]}</span>
+    `);
+  }
+
+  document.write(`
+      <span>].</span>
+    </li>
+  `);
 }
