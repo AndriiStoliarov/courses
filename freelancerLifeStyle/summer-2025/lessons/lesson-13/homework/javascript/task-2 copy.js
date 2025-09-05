2c07170e74412f4565829c864da423fca74b5207
Andrii Stoliarov <andrey159852@gmail.com>
2025-09-05 18:49:32 +0300

    lesson 13 files uploaded

---
+++ freelancerLifeStyle/summer-2025/lessons/lesson-13/homework/javascript/task-2 copy.js
@@ -0,0 +1,155 @@
+"use strict";
+document.write(`
+  <div class="wrapper">
+  <!-- Header -->
+  <header class="header" id="up">
+    <div class="header__container">
+      <div class="header__inner">
+        <h1 class="header__title">Домашне завдання № 12</h1>
+      </div><!-- /. header__inner -->
+    </div><!-- /. header__container -->
+  </header>
+
+  <main class="page">
+    <!-- Tasks -->
+    <div class="tasks">
+      <div class="tasks__container">
+        <div class="tasks__inner">
+          <ul class="tasks__items">
+            <li class="tasks__item">
+              <p><span class="tasks__title">Задача 2.</span> Дано масив 30 випадкових цілих чисел. Підрахувати скільки було обмінів під час сортування змішуванням.
+              </p>
+            </li>
+`);
+if (confirm("Почати тестування?")) {
+    const ELEMENTS_AMOUNT = 30;
+    const RANDOM_NUMBER_FROM = 10, RANDOM_NUMBER_TO = 99;
+    let numbersArray = createArray(ELEMENTS_AMOUNT, RANDOM_NUMBER_FROM, RANDOM_NUMBER_TO);
+    document.write(`
+    <li>
+      <p>
+        <span class="tasks__title">Відповідь:</span>
+        <ul class="list">
+  `);
+    showArray(numbersArray, "Масив випадкових чисел");
+    document.write("<br>");
+    const changeAmountShakerSort = getChangeAmountShakerSort(numbersArray);
+    showArray(numbersArray, "Масив відсортованих чисел");
+    document.write(`
+    <li class="list__item">
+      <span>Кількість обмінів під час сортування: ${changeAmountShakerSort}.</span>
+    </li>
+  `);
+    document.write(`
+        </ul>
+      </p>
+    <li>
+  `);
+    // функция, яка повертає кількість змін при сортуванні массива методом змішування
+    function getChangeAmountShakerSort(array) {
+        let leftIndex = 0, rightIndex = array.length - 1;
+        let changeCounter = 0;
+        while (leftIndex < rightIndex) {
+            let change = false;
+            // рух зліва направо
+            for (let index = leftIndex + 1; index <= rightIndex; index++) {
+                if (array[index - 1] > array[index]) {
+                    const temp = array[index - 1];
+                    array[index - 1] = array[index];
+                    array[index] = temp;
+                    change = true;
+                    changeCounter++;
+                }
+            }
+            rightIndex--;
+            if (change === false)
+                break;
+            // рух з права наліво
+            change = false;
+            for (let index = rightIndex; index > leftIndex; index--) {
+                if (array[index - 1] > array[index]) {
+                    let temp = array[index - 1];
+                    array[index - 1] = array[index];
+                    array[index] = temp;
+                    change = true;
+                    changeCounter++;
+                }
+            }
+            leftIndex++;
+            if (change === false)
+                break;
+        }
+        return changeCounter;
+    }
+    /**
+     * Функція повертає масив випадкових елементів.
+     * @param sizeArray
+     * @param startRange
+     * @param endRange
+     * @returns
+     */
+    function createArray(sizeArray, startRange, endRange) {
+        let array = [];
+        for (let index = 0; index < sizeArray; index++) {
+            array.push(generateRandomNumber(startRange, endRange));
+        }
+        return array;
+    }
+    /**
+     * Функція повертає випадкове число в заданому інтервалі.
+     *
+     * @param {number} startRange - Число для початку интервала.
+     * @param {number} endRange - Число для кінця интервалу.
+     * @returns {number} Випадково згенероване число.
+     */
+    function generateRandomNumber(startRange, endRange) {
+        const RANDOM_NUMBER_FROM = startRange, RANDOM_NUMBER_TO = endRange;
+        const randomNumber = RANDOM_NUMBER_FROM +
+            Math.floor(Math.random() * (RANDOM_NUMBER_TO - RANDOM_NUMBER_FROM + 1));
+        return randomNumber;
+    }
+    /**
+     * Функція, яка виводить масив.
+     *
+     * @param {number[]} array - Масив який треба вивести.
+     * @param {string} caption - Підпис/заголовок до масиву.
+     */
+    function showArray(array, caption) {
+        document.write(`
+    <li class="list__item">
+      <span>${caption}: [</span>
+  `);
+        for (let index = 0; index < array.length; index++) {
+            document.write(`
+      <span>${array[index]}</span>
+    `);
+        }
+        document.write(`
+      <span>].</span>
+    </li>
+  `);
+    }
+}
+document.write(`
+            </ul>
+          </div><!-- /.tasks__inner -->
+        </div><!-- /.tasks__container -->
+      </div><!-- /.tasks -->
+
+      <div class="up  up--left">
+        <div class="up__container">
+          <a class="up__link" href="../index.html">на головну</a>
+        </div>
+      </div>
+    </main><!-- /.page -->
+
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="footer__container">
+        <div class="footer__inner">
+          <div class="footer__text"><span>&#169; </span>2025 Andrii Stoliarov</div>
+        </div>
+      </div><!-- /.footer__inner-->
+    </footer>
+  </div><!-- /.wrapper -->
+`);
---
+++ freelancerLifeStyle/summer-2025/lessons/lesson-13/homework/javascript/task-5.js
@@ -0,0 +1,182 @@
+"use strict";
+document.write(`
+  <div class="wrapper">
+  <!-- Header -->
+  <header class="header" id="up">
+    <div class="header__container">
+      <div class="header__inner">
+        <h1 class="header__title">Домашне завдання № 12</h1>
+      </div><!-- /. header__inner -->
+    </div><!-- /. header__container -->
+  </header>
+
+  <main class="page">
+    <!-- Tasks -->
+    <div class="tasks">
+      <div class="tasks__container">
+        <div class="tasks__inner">
+          <ul class="tasks__items">
+            <li class="tasks__item">
+              <p><span class="tasks__title">Задача 5.</span> Дано масив імен. Застосовуючи відповідне сортування та бінарний пошук визначити, чи є у масиві ім’я «Olga» і під яким індексом.
+              </p>
+            </li>
+`);
+if (confirm("Почати тестування?")) {
+    let namesArray = [
+        "Andrii",
+        "Ivan",
+        "Volodymyr",
+        "Serhii",
+        "Mykola",
+        "Petro",
+        "Maksym",
+        "Taras",
+        "Roman",
+        "Bogdan",
+        "Yevhen",
+        "Ihor",
+        "Olga",
+        "Vitalii",
+        "Yaroslav",
+        "Olena",
+        "Mariia",
+        "Iryna",
+        "Nataliia",
+        "Tetiana",
+    ];
+    document.write(`
+    <li>
+      <p>
+        <span class="tasks__title">Відповідь:</span>
+        <ul class="list">
+  `);
+    showArray(namesArray, "Масив імен");
+    document.write("<br>");
+    changeArrayQuickSort(namesArray, 0, namesArray.length - 1);
+    showArray(namesArray, "Відсортований масив імен");
+    document.write("<br>");
+    const isElementIncludeArray = includeBinarySearch(namesArray, "Olga");
+    const isIndexElementIncludeArray = findIndexBinarySearch(namesArray, "Olga");
+    if (isElementIncludeArray) {
+        document.write(`
+    <li class="list__item">
+      <span>В масиві є ім’я "Olga", під індексом: ${isIndexElementIncludeArray}.</span>
+    </li>
+  `);
+    }
+    else {
+        document.write(`
+    <li class="list__item">
+      <span>В масиві немає такого імені.</span>
+    </li>
+  `);
+    }
+    document.write(`
+        </ul>
+      </p>
+    <li>
+  `);
+    // функция, яка знаходить індекс елемента в упорядкованому масиві.
+    function findIndexBinarySearch(sortedArray, searchElement) {
+        let start = 0, end = sortedArray.length - 1;
+        while (start <= end) {
+            const middle = Math.floor((start + end) / 2);
+            if (sortedArray[middle] === searchElement)
+                return middle;
+            if (sortedArray[middle] < searchElement)
+                start = middle + 1;
+            if (sortedArray[middle] > searchElement)
+                end = middle - 1;
+        }
+        return -1;
+    }
+    // функция, яка перевіряє належність елемента в упорядкованому масиві.
+    function includeBinarySearch(sortedArray, searchElement) {
+        let start = 0, end = sortedArray.length - 1;
+        while (start <= end) {
+            const middle = Math.floor((start + end) / 2);
+            if (sortedArray[middle] === searchElement)
+                return true;
+            if (sortedArray[middle] < searchElement)
+                start = middle + 1;
+            if (sortedArray[middle] > searchElement)
+                end = middle - 1;
+        }
+        return false;
+    }
+    // функция, яка сортує массив швидким методом.
+    function changeArrayQuickSort(array, p, q) {
+        if (p >= q)
+            return;
+        // ділимо на дві частини
+        let i = Partition(array, p, q);
+        // окремо сортуємо першу частину
+        changeArrayQuickSort(array, p, i - 1);
+        // окремо сортуємо другу частину
+        changeArrayQuickSort(array, i + 1, q);
+        // функція, яка ділить фрагмент від p до q на дві частини (<= pivot та > pivot)
+        function Partition(array, p, q) {
+            let pivot = array[q];
+            let i = p - 1;
+            for (let j = p; j < q; j++) {
+                if (array[j] <= pivot) {
+                    i = i + 1;
+                    swap(array, i, j);
+                }
+            }
+            swap(array, i + 1, q);
+            // повертаємо нову позицію pivot
+            return i + 1;
+        }
+        // функция обміну елементів місцями
+        function swap(array, i, j) {
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    /**
+     * Функція, яка виводить масив.
+     *
+     * @param {(number | string)[]} array - Масив який треба вивести.
+     * @param {string} caption - Підпис/заголовок до масиву.
+     */
+    function showArray(array, caption) {
+        document.write(`
+    <li class="list__item">
+      <span>${caption}: [</span>
+  `);
+        for (let index = 0; index < array.length; index++) {
+            document.write(`
+      <span>${array[index]}</span>
+    `);
+        }
+        document.write(`
+      <span>].</span>
+    </li>
+  `);
+    }
+}
+document.write(`
+            </ul>
+          </div><!-- /.tasks__inner -->
+        </div><!-- /.tasks__container -->
+      </div><!-- /.tasks -->
+
+      <div class="up  up--left">
+        <div class="up__container">
+          <a class="up__link" href="../index.html">на головну</a>
+        </div>
+      </div>
+    </main><!-- /.page -->
+
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="footer__container">
+        <div class="footer__inner">
+          <div class="footer__text"><span>&#169; </span>2025 Andrii Stoliarov</div>
+        </div>
+      </div><!-- /.footer__inner-->
+    </footer>
+  </div><!-- /.wrapper -->
+`);
