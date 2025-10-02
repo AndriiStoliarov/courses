043c08f25fcc02489fc1479ce752124d7f2f2fe4
Andrii Stoliarov <andrey159852@gmail.com>
2025-10-02 19:49:37 +0300

    homework 20 task add

--- freelancerLifeStyle/summer-2025/lessons/lesson-20/homework/javascript/task.js
+++ freelancerLifeStyle/summer-2025/lessons/lesson-20/homework/javascript/task.js
@@ -5,56 +5,166 @@ const cars = [
   {
     id: 1,
     make: "Mercedes",
-    year: 2018,
+    releaseYear: 2018,
     price: 25000,
   },
   {
     id: 2,
     make: "Opel",
-    year: 2000,
+    releaseYear: 2000,
     price: 5000,
   },
   {
     id: 3,
     make: "Opel",
-    year: 2004,
+    releaseYear: 2004,
     price: 15000,
   },
   {
     id: 4,
     make: "Audi",
-    year: 2000,
+    releaseYear: 2000,
     price: 7000,
   },
 ];
 
 window.onload = function () {
   const resultList = document.getElementById("resultList");
-
   const list = new List(cars);
+  resultList.append(list.render());
 
-  console.log("list.list :>> ", list.list);
+  const resultFilter = document.getElementById("resultFilter");
+  const filter = new Filter();
+  resultFilter.append(filter.render());
 };
 
 class List {
-  list = [];
-
   constructor(initialList) {
-    this.list = JSON.parse(JSON.stringify(initialList));
+    this.Value = initialList;
   }
 
-  get list() {
+  get Value() {
     return this.list;
   }
 
-  // set list(newList) {
-  //   if (newList.light !== 0) {
-  //     this.list = newList;
-  //   }
-  // }
+  set Value(newList) {
+    if (newList.length !== 0) {
+      this.list = JSON.parse(JSON.stringify(newList));
+    }
+  }
+
+  createUlTag() {
+    const ulTag = document.createElement("ul");
+    ulTag.classList.add("board__list");
+
+    return ulTag;
+  }
+
+  createLiTag() {
+    const liTag = document.createElement("li");
+
+    return liTag;
+  }
 
   render() {
-    console.log("this.list :>> ", this.list);
+    let ulTag = this.createUlTag();
+
+    for (const car of this.list) {
+      const liTag = this.createLiTag();
+      liTag.innerHTML = `<p><span>${car.make}</span> - <span>${car.releaseYear} р.</span> - <span>${car.price}$</span></p>`;
+      ulTag.append(liTag);
+    }
+
+    return ulTag;
+  }
+}
+
+class Filter {
+  constructor() {}
+
+  getObjectValues(key) {
+    this.currentList = new List(cars).Value;
+
+    const values = this.currentList
+      .map((car) => car[key])
+      .filter((item, index, array) => array.indexOf(item) === index);
+
+    return values;
+  }
+
+  createFormTag() {
+    const formTag = document.createElement("form");
+    formTag.className = "form  filter-form";
+
+    return formTag;
+  }
+
+  createFieldsetTag() {
+    const fieldsetTag = document.createElement("fieldset");
+    fieldsetTag.classList.add("filter-form__item");
+
+    return fieldsetTag;
+  }
+
+  createLabelTag(title, paramFor) {
+    const labelTag = document.createElement("label");
+    labelTag.classList.add("filter-form__label");
+    labelTag.setAttribute("for", paramFor);
+    labelTag.innerText = title;
+
+    return labelTag;
+  }
+
+  createSelectTag(paramId) {
+    const selectTag = document.createElement("select");
+    selectTag.classList.add("filter-form__select");
+    selectTag.id = paramId;
+
+    return selectTag;
+  }
+
+  createOptionTag(value) {
+    const optionTag = document.createElement("option");
+
+    if (value) {
+      optionTag.value = value;
+      optionTag.innerText = value;
+    } else {
+      optionTag.value = 0;
+      optionTag.selected;
+    }
+
+    return optionTag;
+  }
+
+  render() {
+    const formTag = this.createFormTag();
+
+    // створюємо фильтр "Марка" та додаем його до загальної форми
+    const fieldsetTagMake = this.createFieldsetTag();
+    fieldsetTagMake.append(this.createLabelTag("Марка:", "makeCar"));
+    const selectTagMake = this.createSelectTag("makeCar");
+    selectTagMake.append(this.createOptionTag());
+    for (const make of this.getObjectValues("make")) {
+      selectTagMake.append(this.createOptionTag(make));
+    }
+    fieldsetTagMake.append(selectTagMake);
+    formTag.append(fieldsetTagMake);
+
+    // створюємо фильтр "Рік випуску" та додаем його до загальної форми
+    const fieldsetTagReleaseYear = this.createFieldsetTag();
+    fieldsetTagReleaseYear.append(
+      this.createLabelTag("Рік випуску:", "releaseYearCar")
+    );
+    const selectTagReleaseYear = this.createSelectTag("releaseYearCar");
+    selectTagReleaseYear.append(this.createOptionTag());
+    for (const releaseYear of this.getObjectValues("releaseYear")) {
+      selectTagReleaseYear.append(this.createOptionTag(releaseYear));
+    }
+    fieldsetTagReleaseYear.append(selectTagReleaseYear);
+    formTag.append(fieldsetTagReleaseYear);
+
+    return formTag;
   }
 }
 
